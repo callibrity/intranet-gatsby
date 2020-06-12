8dad2aede08c168f24327580c10fcb6e3c0f6991
morellaj <alexjamesmorelli@gmail.com>
2020-06-12 12:41:39 -0400

    merge

--- config/aliases.js
+++ config/aliases.js
@@ -1,22 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
-  "@home": "src/components/home",
-  "@navbar": "src/components/navbar",
-  "@people": "src/components/people",
-  "@portfolio": "src/components/portfolio",
-  "@wiki": "src/components/wiki",
-  "@components": "src/components",
-  "@globals": "src/globals",
-  "@hooks": "src/hooks",
-  "@images": "src/images",
-  "@pages": "src/pages",
-  "@constants": "src/globals/constants",
-  "@testConstants": "src/globals/testConstants",
-  "@useAPI": "src/hooks/useAPI",
-  "@api": "src/globals/api",
-  "@contexts": "src/globals/contexts",
-  "@styles": "src/globals/styles",
-=======
   "@home": "src/components/home/",
   "@navbar": "src/components/navbar/",
   "@people": "src/components/people/",
@@ -26,5 +8,4 @@ module.exports = {
   "@hooks": "src/hooks/",
   "@images": "src/images/",
   "@pages": "src/pages/",
->>>>>>> 5fcc204cad8d3d01c1b14e12300774fd9879b3e9
 }
