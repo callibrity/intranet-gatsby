18d3bb0dd31148980f26d62afa510f7102afa6c0
morellaj <alexjamesmorelli@gmail.com>
2020-06-17 08:37:30 -0400

    Removed local markdown pages

--- src/pages/wiki.js
+++ src/pages/wiki.js
@@ -1,51 +1,11 @@
 import React from "react"
-import { Link } from "gatsby"
 import styled from "styled-components"
-import { useStaticQuery, graphql } from "gatsby"
 
 
 export default function Wiki() {
-  const data = useStaticQuery(graphql`
-  query {
-    postgres {
-      allWikitestsList {
-        slug
-        title
-      }
-    }
-  }
-`)
 
-  const pageList = data.postgres.allWikitestsList.map(({title, slug}) => 
-    <StyledLink to={`/wiki/${slug}`} key={title}>{title}</StyledLink>
-  )
-
+  
   return (
-    <Container>
-      <Section>
-        <Label>Pages</Label>
-        <ListContainer>{pageList}</ListContainer>
-      </Section>
-    </Container>
+    <div></div>
   )
 }
-
-const Container = styled.div`
-  display: flex;
-`
-
-const Section = styled.div``
-
-const Label = styled.div`
-  font-size: 30px;
-  font-weight: 700;
-`
-
-const ListContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-`
-
-const StyledLink = styled(Link)`
-  color: blue;
-`
