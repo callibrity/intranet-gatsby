cf06cf4c7b739e4b639bfb6ff21f04a12de74ad7
Arielle <arielleferre@gmail.com>
2020-07-07 12:27:48 -0400

    time tracker refactoring

--- src/components/home/TimeTracker.js
+++ src/components/home/TimeTracker.js
@@ -1,54 +1,47 @@
 import React from "react"
 import styled from "styled-components"
+import Section from "@home/Section"
 
-import {EmployeeTimeTracking} from "@globals/constants"
+import { EmployeeTimeTracking } from "@globals/constants"
 
+const Hours = ({ title, hours }) => (
+  <Section color="green" label={title} className="TimeTracker-Hours">
+    {hours.map(({ label, amount }, index) => (
+      <div key={index} className="TimeTracker-Hours-details">
+        <span>{label}</span>
+        <span>{amount}</span>
+      </div>
+    ))}
+  </Section>
+)
 
-export default function TimeTracker() {
-  const {billable, training, pto} = EmployeeTimeTracking
-  const remainingTraining = training.totalHours - training.currentHours
+const TimeTracker = () => {
+  const { data } = EmployeeTimeTracking
 
   return (
     <Container>
-      <table>
-        <tr>
-          <th colSpan="2">Target Hours</th>
-          <th colSpan="2">Growth Time</th>
-          <th colSpan="2">PTO</th>
-        </tr>
-        <tr>
-          <td>Current Hours: </td>
-          <td>{billable.currentHours}</td>
-          <td>Hours Used: </td>
-          <td>{training.currentHours}</td>
-          <td>Hours Used: </td>
-          <td>{pto.hoursUsed}</td>
-        </tr>
-        <tr>
-          <td>Target Hours: </td>
-          <td>{billable.currentTarget}</td>
-          <td>Hours Remaining: </td>
-          <td>{remainingTraining}</td>
-          <td>Hours Remaining: </td>
-          <td>{pto.hoursRemaining}</td>
-        </tr>
-        <tr>
-          <td>Total Target: </td>
-          <td>{billable.totalTarget}</td>
-          <td>Total Growth: </td>
-          <td>{training.totalHours}</td>
-          <td>Hours not yet accrued: </td>
-          <td>{pto.hoursNotAccrued}</td>
-        </tr>
-      </table>
+      {data.map(({ title, hours }, index) => (
+        <Hours key={index} title={title} hours={hours} />
+      ))}
     </Container>
   )
 }
 
+export default TimeTracker
+
 const Container = styled.div`
   display: flex;
-  justify-content: space-between;
-  table {
-    width: 100%;
+  justify-content: center;
+  flex-wrap: wrap;
+
+  .TimeTracker-Hours {
+    margin: 8px;
+    width: 500px;
+    min-width: 300px;
+
+    .TimeTracker-Hours-details {
+      display: flex;
+      justify-content: space-between;
+    }
   }
 `
