7e144ca32157068c03980467c816a2d1451bc101
Collin Johnson <collin.johnson@callibrity.com>
2020-08-12 19:23:01 -0400

    test passing for indextest with contextprovider

--- src/components/home/TimeTracker.test.tsx
+++ src/components/home/TimeTracker.test.tsx
@@ -2,13 +2,11 @@ import React, { useContext } from 'react';
 import { mount } from 'enzyme';
 import TimeTracker from '@home/TimeTracker';
 import { UserContext } from '@globals/contexts';
-import GlobalProvider from '@components/GlobalProvider';
-
-const mockSetUserRole = jest.fn();
 
 const contextProps = {
   userRole: 'Developer',
 };
+
 describe('TimeTracker component', () => {
   it('renders two cards for user role Developer', () => {
     const wrapper = mount(
