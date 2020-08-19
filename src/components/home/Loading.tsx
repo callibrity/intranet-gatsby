83e68a93d265d670c2060dc1720b269556315680
Connor <cmason@callibrity.com>
2020-08-19 11:52:42 -0400

    spinner working

--- src/components/home/Loading.tsx
+++ src/components/home/Loading.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
-import { Container, Image, Row, Col, Spinner } from 'react-bootstrap';
+import { Spinner } from 'react-bootstrap';
 import styled from 'styled-components';
-import Img from 'gatsby-image';
 
 const loading = () => (
-    <Container>
-      <Row className="justify-content-center align-items-center" style={{minHeight: '100%'}}>
-        <Col xs={12} sm={4} md={4} className="justify-content-center">
-          <Spinner animation="border" role="status" style={{ height: '200px', width: '200px', alignSelf: 'center' }} />
-        </Col>
-      </Row>
-    </Container>
+  <SpinnerContainer>
+    <Spinner animation="border" role="status" style={{ height: '150px', width: '150px' }} />
+  </SpinnerContainer>
 );
 
 export default loading;
+
+const SpinnerContainer = styled.div`
+  display: flex;
+  align-items: center; 
+  justify-content: center;
+`;
