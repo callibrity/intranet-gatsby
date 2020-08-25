e12f7e71cca78a4e4978eff3e8c7f6fa2ebcccb7
Collin Johnson <collin.johnson@callibrity.com>
2020-08-25 13:28:47 -0400

    changed styling on searchbar

--- src/components/home/EmployeeSearch.tsx
+++ src/components/home/EmployeeSearch.tsx
@@ -1,24 +1,25 @@
 import React, { useState } from 'react';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
+import { IconContext } from 'react-icons';
 import { MdSearch } from 'react-icons/md';
 
-const EmployeeSearch = (props) => {
-  return (
-    <InputGroup>
+const EmployeeSearch = (props) => (
+  <InputGroup>
+    <IconContext.Provider value={{ color: 'grey' }}>
       <FormControl
-        aria-label="Search for an employee"
+        aria-label="Type at least two characters to pull up developers"
         alt="search bar"
         value={props.text}
         placeholder="Type at least two characters to pull up developers..."
         onChange={(e) => props.setText(e.target.value)}
-        style={{ height: '100%' }}
+        style={{ height: '100%', borderTopLeftRadius: 100, borderBottomLeftRadius: 100 }}
       />
       <InputGroup.Append>
-        <InputGroup.Text><MdSearch /></InputGroup.Text>
+        <InputGroup.Text style={{ backgroundColor: 'white', borderBottomRightRadius: 100, borderTopRightRadius: 100 }}><MdSearch /></InputGroup.Text>
       </InputGroup.Append>
-    </InputGroup>
-  );
-};
+    </IconContext.Provider>
+  </InputGroup>
+);
 
 export default EmployeeSearch;
