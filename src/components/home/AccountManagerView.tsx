d39cba4001cb75ba3517c493b7be74bd1eb8987f
Alex Morelli <alexmorelli@CVG-C02RK01YG8WQ.local>
2020-08-26 15:48:21 -0400

    refactored components

---
+++ src/components/home/AccountManagerView.tsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { getAllEmployeeMetrics } from '@api/serviceCalls';
+import { Container } from 'react-bootstrap';
+import EmployeeSearch from './EmployeeSearch';
+import { BillableTypes, GrowthTypes, ImageQuery } from '@globals/types';
+import DeveloperCardRow from './DeveloperCardRow';
+import { reactChildren } from '@globals/types';
+
+type EmployeeTypes = (BillableTypes & GrowthTypes & {employeeName: string, employeeId: string})[];
+
+const AccountManagerView = ({data} : ImageQuery) => {
+  const [userData, setUserData] = useState<EmployeeTypes>([]);
+  const [searchString, setSearchString] = useState('');
+  const [favoritesList, setFavoritesList] = useState([]);
+
+  const images = data.mugs.nodes.map((node) => node.childImageSharp.fixed);
+
+  useEffect(() => {
+    getAllEmployeeMetrics(setUserData, console.log);
+  }, []);
+
+  const isLocked = (employeeId: string): boolean => favoritesList.includes(employeeId);
+
+  const favoriteListToggleHandler = (employeeId: string) => {
+    if (isLocked(employeeId)) { 
+      setFavoritesList(favoritesList.filter((favoriteId) => favoriteId !== employeeId)); 
+    } else { 
+      setFavoritesList([...favoritesList, employeeId]); 
+    }
+  };
+
+  const favoriteList : reactChildren  = [];
+  const notFavoriteList : reactChildren = [];
+
+  userData.forEach((developer) => {
+    const { employeeId, employeeName } = developer;
+    const isFavorite = isLocked(employeeId);
+    const show = searchString.length > 1 && employeeName.toLowerCase().includes(searchString.toLowerCase());
+    if( isFavorite || show){
+      const img = images.find((image) => image.originalName === `${employeeId}.jpg`) || data.mugPlaceholder.childImageSharp.fixed;
+      const UserComponent = <DeveloperCardRow 
+        key={employeeId} 
+        developerData={developer}  
+        isLockedRow={isFavorite} 
+        lockToggle={favoriteListToggleHandler} 
+        img={img}
+      />
+      if (isFavorite){
+        favoriteList.push(UserComponent);
+      } else {
+        notFavoriteList.push(UserComponent);
+      }
+    }
+  })
+
+  return (
+      <>
+        <Container style={{ marginBottom: 16 }}>
+          <EmployeeSearch text={searchString} setText={setSearchString} />
+        </Container>
+        { favoriteList }
+        <SeparateFavorites />
+        { notFavoriteList }
+      </>
+  );
+};
+
+export default AccountManagerView;
+
+const SeparateFavorites = styled.div`
+  display: block;
+  border-top: 3px solid black;
+  margin: 1em 0;
+`;
+
---
+++ src/globals/helperFunctions.ts
@@ -0,0 +1,35 @@
+import { BillableTypes, GrowthTypes } from '@globals/types';
+
+export const billableConversion = ({currentHours, currentTarget, totalTarget } : BillableTypes ) => {
+  return [
+    {
+      label: 'Current Hours',
+      value: currentHours
+    },
+    {
+      label: 'Current Target',
+      value: currentTarget
+    },
+    {
+      label: 'Total Target',
+      value: totalTarget
+    }
+  ]
+}
+
+export const growthConversion = ({hoursUsed, hoursRemaining, totalGrowth } : GrowthTypes ) => {
+  return [
+    {
+      label: 'Hours Used',
+      value: hoursUsed
+    },
+    {
+      label: 'Hours Remaining',
+      value: hoursRemaining
+    },
+    {
+      label: 'Total Growth',
+      value: totalGrowth
+    }
+  ]
+}
