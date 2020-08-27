a94fb90293a4b4347ba25632622f651a58a1fc74
Alex Morelli <alexmorelli@CVG-C02RK01YG8WQ.local>
2020-08-27 15:52:23 -0400

    improved login

--- src/components/GlobalProvider.tsx
+++ src/components/GlobalProvider.tsx
@@ -5,16 +5,40 @@ import Navbar from './navbar/Navbar';
 import GlobalStyle from './GlobalStyle';
 import { reactChildren } from '@globals/types';
 import { globalHistory } from '@reach/router';
+import { googleClientId } from '@globals/constants';
+import { getEmployeeDetails } from '@api/serviceCalls';
+import { setJwt } from '@api/api';
+import { navigate } from 'gatsby';
+import { useGoogleLogin, GoogleLoginResponse } from 'react-google-login';
 
 export const Provider = ({ children }: { children: reactChildren }) => {
   const [username, setUsername] = useState<string>(null);
   const [userEmail, setUserEmail] = useState<string>(null);
   const [userRole, setUserRole] = useState<string>(null);
-  const contextObject = { username, setUsername, userEmail, setUserEmail, userRole, setUserRole };
+  const [signInLoaded, setSignInLoaded] = useState<boolean>(false);
+  const { signIn } = useGoogleLogin({
+    clientId: googleClientId,
+    onSuccess: ({ tokenId, profileObj: { name, email } }: GoogleLoginResponse) => {
+      setJwt(tokenId);
+      setUsername(name);
+      setUserEmail(email);
+      getEmployeeDetails(setUserRole, console.log);
+    },
+    onFailure: () => console.log("sign in failed"),
+    onAutoLoadFinished: (signedIn) => {
+      setSignInLoaded(true);
+      if (!signedIn) {
+        navigate('/login');
+      }
+    },
+    isSignedIn: true
+  });
+
+  const contextObject = { username, setUsername, userEmail, setUserEmail, userRole, setUserRole, signIn };
 
   useEffect(() => {
     return globalHistory.listen((result) => {
-      console.log('location:', result.location.pathname);
+      //console.log('location:', result.location.pathname);
     })
   })
 
@@ -22,7 +46,7 @@ export const Provider = ({ children }: { children: reactChildren }) => {
     <UserContext.Provider value={contextObject}>
       <GlobalStyle />
       <Navbar />
-      {username ? children : <Login />}
+      {signInLoaded ? children : null}
     </UserContext.Provider>
   );
 };
