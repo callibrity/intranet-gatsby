9cb48c944616dc1d470d31c3f580160dd1291781
Alex Morelli <alexmorelli@CVG-C02RK01YG8WQ.local>
2020-09-01 08:08:19 -0400

    fixed another typo

--- src/components/navbar/Navbar.test.tsx
+++ src/components/navbar/Navbar.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import Navbar from './NavBar';
+import Navbar from './Navbar';
 import { UserContext } from '@globals/contexts';
 import { mockContextValue } from '@globals/testConstants';
 
