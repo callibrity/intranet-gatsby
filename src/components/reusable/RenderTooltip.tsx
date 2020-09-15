213a7b7c162081a28ab65c9edee0952fec63a018
Collin Johnson <collinjohnson@cvg-c02sf1q7g8wm.home>
2020-09-15 08:41:09 -0400

    changed imports from spread to explicit

--- src/components/reusable/RenderTooltip.tsx
+++ src/components/reusable/RenderTooltip.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Tooltip } from 'react-bootstrap';
+import Tooltip from 'react-bootstrap/Tooltip';
 import { OverlayInjectedProps } from 'react-bootstrap/Overlay';
 
 const RenderTooltip = (props: OverlayInjectedProps, updatedAt: string) => (
